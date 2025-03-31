ed8460cd358330def1a3d910b050020690bd6467
Z Sullivan <0442338+zothsu@users.noreply.github.com.>
2025-03-31 00:19:22 +0000

    add hidden files

---
+++ bash_aliases.md
@@ -0,0 +1,24 @@
+# System Updates
+alias updt='sudo apt update'
+alias upgrd='sudo apt upgrade -y'
+
+# Better ls
+alias ls='ls -lish --color=auto'
+
+# Docker Shortcuts
+alias d='docker'
+alias dc='docker compose'
+alias dcu='docker compose up -d'
+alias dcd='docker compose down'
+alias dcl='docker compose logs -f'
+alias dps='docker ps -a'
+
+# Server & Maintenance
+alias cls='clear'
+alias v='vim'
+alias reload='source ~/.bashrc'
+alias ipinfo='ip a | grep inet'
+
+# Nicer navigation
+alias ..='cd ..'
+alias ...='cd ../..'
---
+++ gitignore.sh
@@ -0,0 +1,55 @@
+# standard
+**.DS_Store
+dynamic-exports.zsh
+.cache
+
+
+# logs and pid
+.local/share/**
+.local/state/**
+logs
+*.log
+
+
+# staging area for cloned repos for projects
+repos
+
+dotfiles/nvim/.config/nvim/plugin/*
+
+# Compiled source #
+###################
+*.com
+*.class
+*.dll
+*.exe
+*.o
+*.so
+
+# Packages #
+############
+# it's better to unpack these files and commit the raw source
+# git has its own built in compression methods
+*.7z
+*.dmg
+*.gz
+*.iso
+*.jar
+*.rar
+*.tar
+*.zip
+
+# Logs and databases #
+######################
+*.log
+*.sql
+*.sqlite
+
+# OS generated files #
+######################
+.DS_Store
+.DS_Store?
+._*
+.Spotlight-V100
+.Trashes
+ehthumbs.db
+Thumbs.db
